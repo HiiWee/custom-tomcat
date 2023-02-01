d5f2d5831b1cbf0f5e919c7c103f134fc384b2e6
HiiWee <wpdlzhf159@gmail.com>
2023-02-02 01:28:39 +0900

    refactor: 디폴트 포트가 8080이므로 필요없는 포트 설정 삭제

--- tomcat-server/src/net/hiiwee/WebServerLauncher.java
+++ tomcat-server/src/net/hiiwee/WebServerLauncher.java
@@ -9,12 +9,14 @@ public class WebServerLauncher {
     public static void main(String[] args) throws LifecycleException {
         String webappDirLocation = "webapp/";
         Tomcat tomcat = new Tomcat();
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
 
-        tomcat.setPort(Integer.parseInt(webPort));
+//         디폴트 port 설정이 8080임
+//        String webPort = System.getenv("PORT");
+//        if (webPort == null || webPort.isEmpty()) {
+//            webPort = "8080";
+//        }
+//         tomcat.setPort(Integer.parseInt(webPort));
+
         Connector connector = tomcat.getConnector();
         connector.setURIEncoding("UTF-8");
         tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
